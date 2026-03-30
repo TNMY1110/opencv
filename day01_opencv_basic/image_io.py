4b8e862ea2f8fbe7c599ebe5b091b34b9e610cd4
unknown <searain1110@gmail.com>
2026-03-30 12:53:34 +0900

    원격에서 샘플 파일 가지고 오는 코드 추가

---
+++ day01_opencv_basic/image_io.py
@@ -0,0 +1,40 @@
+import cv2 as cv
+import urllib.request
+import sys
+import os
+
+def get_sample(filename, flags=cv.IMREAD_COLOR):
+    if not os.path.exists(filename):
+        url = f"https://raw.githubusercontent.com/opencv/opencv/master/samples/data/{filename}"
+        urllib.request.urlretrieve(url, filename)
+    
+    return cv.imread(filename, flags)
+
+# img = cv.imread("./samples/starry_night.jpg")
+# img_gray = cv.imread("./samples/starry_night.jpg", cv.IMREAD_GRAYSCALE)
+img = get_sample("orange.jpg")
+img_gray = get_sample("orange.jpg", cv.IMREAD_GRAYSCALE)
+
+if img is None:
+    sys.exit("Could not read the image.")
+
+cv.imshow("Display window", img)
+
+# 이미지 정보 확인
+print("=== 컬러 이미지 ===")
+print(f"shape: {img.shape}") # (높이, 너비, 채널)
+print(f"shape: {img.dtype}") # 파일의 형태
+print(f"shape: {img.size}") # 전체 픽셀 수(높이 * 너비 * 채널)
+
+print("=== 그레이 스케일 이미지 ===")
+print(f"shape: {img_gray.shape}") # (높이, 너비, 채널)
+print(f"shape: {img_gray.dtype}") # 파일의 형태
+print(f"shape: {img_gray.size}") # 전체 픽셀 수(높이 * 너비 * 채널)
+
+# 창 닫기
+k = cv.waitKey(0)
+
+# 파일 저장
+if k == ord("s"):
+    cv.imwrite("./save/orange.png", img)
+    cv.imwrite("./save/orange_gray.png", img_gray)
