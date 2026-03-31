f917ac36f850eed3a75ec0b48ffa6c89357188bf
unknown <searain1110@gmail.com>
2026-03-31 16:40:35 +0900

    Day2 과제1: 고정 이진화 트랙바

--- day02_opencv_color/step1_global_thresh.py
+++ day02_opencv_color/step1_global_thresh.py
@@ -16,16 +16,26 @@ def nothing(x):
 
 cv.namedWindow('thr', cv.WINDOW_AUTOSIZE)
 cv.createTrackbar('Global_thr', 'thr', 127, 255, nothing)
+mode = '0 : OFF \n1 : ON'
+cv.createTrackbar(mode, 'thr', 0, 1, nothing)
 
 img = get_sample('sudoku.png', cv.IMREAD_GRAYSCALE)
 assert img is not None, "file could not be read, check with os.path.exists()"
+th1 = np.zeros_like(img)
 
 while(1):
     # global thresholding
     Glo_thr = cv.getTrackbarPos('Global_thr', 'thr')
-    ret, th1 = cv.threshold(img, Glo_thr, 255, cv.THRESH_BINARY)
+    mode_val = cv.getTrackbarPos('0 : OFF \n1 : ON', 'thr')
 
-    cv.imshow('thr', th1)
+    if  mode_val == 0:
+        ret, th1 = cv.threshold(img, Glo_thr, 255, cv.THRESH_BINARY)
+
+    elif  mode_val == 1:
+        ret, th1 = cv.threshold(img, Glo_thr, 255, cv.THRESH_BINARY_INV)
+
+    result = np.hstack([img, th1])   # 위쪽 행
+    cv.imshow('thr', result)
 
     k = cv.waitKey(5) & 0xFF
 
