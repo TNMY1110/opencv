7398c426cf203ff814c154478a8c5517ded376c2
unknown <searain1110@gmail.com>
2026-03-31 16:29:17 +0900

    Day2 과제1: 고정 이진화 트랙바

---
+++ day02_opencv_color/step1_global_thresh.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+import numpy as np
+import urllib.request
+import os
+from matplotlib import pyplot as plt
+
+def get_sample(filename, flags=cv.IMREAD_COLOR):
+    if not os.path.exists(filename):
+        url = f"https://raw.githubusercontent.com/opencv/opencv/master/samples/data/{filename}"
+        urllib.request.urlretrieve(url, filename)
+    
+    return cv.imread(filename, flags)
+
+def nothing(x):
+    pass
+
+cv.namedWindow('thr', cv.WINDOW_AUTOSIZE)
+cv.createTrackbar('Global_thr', 'thr', 127, 255, nothing)
+
+img = get_sample('sudoku.png', cv.IMREAD_GRAYSCALE)
+assert img is not None, "file could not be read, check with os.path.exists()"
+
+while(1):
+    # global thresholding
+    Glo_thr = cv.getTrackbarPos('Global_thr', 'thr')
+    ret, th1 = cv.threshold(img, Glo_thr, 255, cv.THRESH_BINARY)
+
+    cv.imshow('thr', th1)
+
+    k = cv.waitKey(5) & 0xFF
+
+    if k == ord('q'):
+        break
+
+cv.destroyAllWindows()
+
